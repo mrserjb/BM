87764b63157842d7679308faced5b331b19b0989
Sergey Baryshev <sergeybaryshev@MacBookSerj.local>
2019-08-21 00:52:45 +0300

    чето шрифт не подцепить по нормальному

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,14 @@
 import '../src/style.css'
+<style>
+@import url('https://fonts.googleapis.com/css?family=Montserrat:400,600,800,900&display=swap&subset=cyrillic');
+</style>
+
 
 function Home() {
     return (
     
         <div className={'welcome'}>
-            Купля-продажа готового бизнеса, франшиз и недвижемости. Консалтинг,коучинг
+            Купля-продажа готового бизнеса, франшиз и недвижемости. Консалтинг, коучинг
         </div>
     )
 }
