e37894ab24be275311930fd3c1edba21a41ffcde
Sergey Baryshev <sergeybaryshev@MacBookSerj.local>
2019-08-20 18:28:32 +0300

    попытка вставить тег body

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,22 @@
 function Home() {
     return (
+        <body>
         <div>
             <style jsx>{`
+                body{
+                    background-color: black;
+                }
                 div {
+                    background-color: black;
                     color: tomato;
+                    font-size: 40px;
                 }
             `}</style>
 
             Welcome to Next.js!
         </div>
-    )
+        </body>
+            )
 }
 
 export default Home
