d1c65a40cd3f7117c126cf25bc6854453d1e3f52
Sergey <stretched.8@gmail.com>
2019-09-27 19:03:20 +0300

    поменял емайл

--- pages/index.jsx
+++ pages/index.jsx
@@ -168,7 +168,6 @@ export default function Home() {
                         {('Телефон: +7 (963) 347-3675, +7 (962) 883-9222')}<br />
                         {('E-mail: ek@бизнесмаркет.com, ks@бизнесмаркет.com')}<br /><br />
                         <a href="https://www.avito.ru/user/b13f450aaa32a0663ee99dca74b65902/profile?id=1773264284&src=item">НАШИ ЛОТЫ</a><br/><br/>
-                        <button className="contact" onClick={() => setModalIsOpen(true)} >Обратная связь</button><button className="contact2" onClick={() => setModalIsOpen(true)} >Обратная связь</button>
                     </p>
                 </div>
                 <LogoImage height={50} />
